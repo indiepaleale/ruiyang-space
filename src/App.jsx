4ae9f8b2b98e845373d6a4a7a373f776909e1bde
Ruiyang Wang <wry0416@outlook.com>
2025-02-18 02:20:24 +0000

    fix: switch from BrowserRouter to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Panel from './components/Panel';
 import Background from './components/Background';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './styles/LandingPage.css';
+
+// Use this for BrowserRouter ! Not working with github pages
+// May need to configure basename in BrowserRouter
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function App() {
 
   return (
     <div className='landing-page'>
-      <Router basename="/ruiyang-space">
+      <Router>
         <Panel />
 
       </Router>
